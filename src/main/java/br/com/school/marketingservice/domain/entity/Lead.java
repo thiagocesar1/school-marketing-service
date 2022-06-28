2e4ae36b4c624eff6a86a1371aa7ffa07ccf28bf
Thiago Alvares <thiago.cesaralvares@gmail.com>
2022-06-28 19:21:07 -0300

    ControllerAdvice criado para tratar mensagens geradas pelo Spring Validation.

--- src/main/java/br/com/school/marketingservice/domain/entity/Lead.java
+++ src/main/java/br/com/school/marketingservice/domain/entity/Lead.java
@@ -6,7 +6,8 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -19,11 +20,12 @@ public class Lead {
     private String id;
 
     @Field
-    @NotEmpty
+    @NotBlank(message = "Name is empty")
     private String name;
 
     @Field
-    @NotEmpty
+    @NotBlank(message = "Email is empty.")
+    @Email(message = "Invalid email.")
     private String email;
 
     @Field
